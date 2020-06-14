42dbd586b1e8c851fba5b22ba56f9b4e4b3ed695
Haley Ferro <haleyvferro@gmail.com>
2020-06-14 14:25:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,12 @@ def introduction(Haley)
 end
 
 def introduction_with_language(Haley, language)
-  puts ""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
